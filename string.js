3d309f38fddb393e74b5c4175e448faf679123b8
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 00:14:53 +0530

    problems are done on null and unidentified values

---
+++ string.js
@@ -0,0 +1,19 @@
+var myString = "JavaScript is cool"
+console.log(myString)
+console.log(typeof(myString))
+
+
+var mySting2 = '\I\'m gonna learn JavaScript'
+console.log(mySting2)
+
+
+var myString3 = "This is a \"test\" String"
+console.log(myString3)
+
+
+var myString4 = "This is another /'test/' string"
+console.log(myString4)
+
+
+var myString5 = "How about this string??"
+console.log(myString5)
